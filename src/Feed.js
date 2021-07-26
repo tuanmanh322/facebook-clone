7022074515f11fcf32c18e20d9a8a9ad8b883734
tuanmanh322 <tuanmanh322@gmail.com>
2021-07-26 07:54:28 +0700

    test

--- src/Feed.js
+++ src/Feed.js
@@ -1,42 +1,45 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "./Feed.css";
-import MessageSender from './MessageSender.js';
-import Post from './Post';
-import StoryReel from './StoryReel';
+import MessageSender from "./MessageSender.js";
+import Post from "./Post";
+import StoryReel from "./StoryReel";
 import db from "./firebase";
 
 function Feed() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    const mountedRef = useRef(true);
+  const mountedRef = useRef(true);
 
-    useEffect(() => {
+  useEffect(() => {
+    console.log(db);
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    );
 
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => ({id: doc.id, data: doc.data()})))
-        ))
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+  // const timestamp = new Date();
+  // const username = "One punch man";
 
-        return () => {mountedRef.current = false}
-
-    }, []);
-    // const timestamp = new Date();
-    // const username = "One punch man";
-
-    return <div className="feed">
-        {/* Story reef */}
-        <StoryReel/> {/* MessageSender*/}
-        <MessageSender/> {/* Post*/}
-         {posts.map((post) => (
-             <Post 
-             Key={post.id}
-             profilePic={post.data.profilePic}
-             message = {post.data.message}
-             timestamp ={post.data.timestamp}
-             username = {post.data.username}
-             image = {post.data.image}
-             />
-         ))}
+  return (
+    <div className="feed">
+      {/* Story reef */}
+      <StoryReel /> {/* MessageSender*/}
+      <MessageSender /> {/* Post*/}
+      {posts.map((post) => (
+        <Post
+          Key={post.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
+  );
 }
 
 export default Feed;
